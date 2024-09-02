ef9c0e66cfa24eb4266d571aa0879a46c4648511
Batax093 <lifeisggwp18@gmail.com>
2024-09-02 21:11:13 +0700

    Deploy v15

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import Home from "./pages/home/Home";
 import AdminLogin from "./pages/admin/AdminLogin";
 import "./App.css";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { ParallaxProvider } from "react-scroll-parallax";
 import { Toaster } from "react-hot-toast";
 
+import { useAuthContext } from "./context/AuthContext";
+
 function App() {
+  const { authUser } = useAuthContext();
+
   return (
     <>
       <Toaster />
       <ParallaxProvider>
         <div className="h-full flex items-center justify-center">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/theemarloes" element={<AdminLogin />} />
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/theemarloes"
+              element={authUser ? <Navigate to={"/"} /> : <AdminLogin />}
+            />
           </Routes>
         </div>
       </ParallaxProvider>
